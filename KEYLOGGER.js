1e8a46c4def4c278476e4396ca8ee07c460414a7
spencercz <48261199+spencercz@users.noreply.github.com>
2021-11-05 12:12:53 -0700

    Update KEYLOGGER.js

--- KEYLOGGER.js
+++ KEYLOGGER.js
@@ -1,6 +1,24 @@
-for (let i = 1; i < 6; i++){
-  document.getElementById("Key_" + String(i)).addEventListener("click", function() {
-  var now = new Date();
-  console.log("Key " + i + ": " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
-  });
- }
+document.getElementById("Key_1").addEventListener("click", function() {
+var now = new Date();
+console.log("Key_1" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+}
+
+document.getElementById("Key_2").addEventListener("click", function() {
+var now = new Date();
+console.log("Key_2" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+}
+
+document.getElementById("Key_3").addEventListener("click", function() {
+var now = new Date();
+console.log("Key_3" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+}
+
+document.getElementById("Key_4").addEventListener("click", function() {
+var now = new Date();
+console.log("Key_4" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+}
+
+document.getElementById("Key_5").addEventListener("click", function() {
+var now = new Date();
+console.log("Key_5" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+}
