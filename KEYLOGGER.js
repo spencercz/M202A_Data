1446238cc11b8ef00066508593b7cea71d7a1175
spencercz <48261199+spencercz@users.noreply.github.com>
2021-11-05 12:30:29 -0700

    Update KEYLOGGER.js

--- KEYLOGGER.js
+++ KEYLOGGER.js
@@ -2,7 +2,9 @@ window.onload = function(){
   for(let i = 1; i < 6; i++){
     document.getElementById("Key_" + i).addEventListener("click", function() {
     var now = new Date();
-    console.log("Key_" + i + ": " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + ":" + now.getMilliseconds());
+    var info = "Key_" + i + ": " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + ":" + now.getMilliseconds()"
+    console.log(info);
+    document.getElementById("data").append(data);
   });
   }
 }
