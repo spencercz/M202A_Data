2f4b333ee9d449e5a530250566ef032713f5a982
spencercz <48261199+spencercz@users.noreply.github.com>
2021-11-05 12:16:07 -0700

    Update KEYLOGGER.js

--- KEYLOGGER.js
+++ KEYLOGGER.js
@@ -1,24 +1,26 @@
-document.getElementById("Key_1").addEventListener("click", function() {
-var now = new Date();
-console.log("Key_1" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
-});
+window.onload = function(){
+  document.getElementById("Key_1").addEventListener("click", function() {
+  var now = new Date();
+  console.log("Key_1" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+  });
 
-document.getElementById("Key_2").addEventListener("click", function() {
-var now = new Date();
-console.log("Key_2" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
-});
+  document.getElementById("Key_2").addEventListener("click", function() {
+  var now = new Date();
+  console.log("Key_2" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+  });
 
-document.getElementById("Key_3").addEventListener("click", function() {
-var now = new Date();
-console.log("Key_3" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
-});
+  document.getElementById("Key_3").addEventListener("click", function() {
+  var now = new Date();
+  console.log("Key_3" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+  });
 
-document.getElementById("Key_4").addEventListener("click", function() {
-var now = new Date();
-console.log("Key_4" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
-});
+  document.getElementById("Key_4").addEventListener("click", function() {
+  var now = new Date();
+  console.log("Key_4" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+  });
 
-document.getElementById("Key_5").addEventListener("click", function() {
-var now = new Date();
-console.log("Key_5" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
-});
+  document.getElementById("Key_5").addEventListener("click", function() {
+  var now = new Date();
+  console.log("Key_5" + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+  });
+}
